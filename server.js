d9f3f008a84a73fe605a8a96c744a0331978e7c7
John Mcilroy <john_mcilroy@hotmail.co.uk>
2019-08-22 01:27:56 +0100

    Hosting Test #001

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const connectDB = require('./config/connectDB');
 const fileUpload = require('express-fileupload')
 
+const path = require('path');
+
 const app = express();
 
 // BodyParsere Middleware
@@ -19,4 +21,12 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/records', require('./routes/api/records'));
 app.use('/api/upload', require('./routes/api/upload'));
 
-app.listen(keys.PORT, () => console.log(`Listening on port: ${keys.PORT}`));
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+app.listen(keys.PORT);
