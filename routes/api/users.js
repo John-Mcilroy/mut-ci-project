50256d43c8fd7af21d44b311f35263cb383cfebc
John Mcilroy <john_mcilroy@hotmail.co.uk>
2019-09-04 00:20:29 +0100

    Bugfix

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator/check');
-const keys = require('../../config/keys');
 
 const User = require('../../models/user-models/User');
 
